5a5251554b5bf6e7b6b5dc18f8c974833dd8a5be
nicoleonard <nikolasleonard@gmail.com>
2023-03-28 07:41:43 -0300

    creados archivos del repositorio

---
+++ js/Ejercicios Trello/Arreglodeproductos.js
@@ -0,0 +1,9 @@
+/* crear un script (en el repo de conceptos de JS o realizar un nuevo repo) Con el siguiente array de objetos, crear una funcion que permita dibujar una tabla con las columnas Producto - categoria - precio. Realizar las siguientes tareas:
+
+Mostrar la tabla completa
+
+filtrar el arreglo de productos por ‘Protectores solares' y mostrar la tabla filtrada.
+
+Buscar un producto serum y mostrarlo por pantalla,
+
+Buscar un producto 'Bruma’ y mostrar un mensaje adecuado para el usuario si el producto no existe en el array.*/
---
+++ js/Ejercicios Trello/Piedrapapeltijera.js
@@ -0,0 +1,61 @@
+/* crear un script (en el repo de conceptos de JS o realizar un nuevo repo) con toda la logica necesaria que solicite al usuario un movimiento (piedra - papel - tijera) la PC elija aleatoriamente un movimiento y al final mostrar por pantalla quien gano, luego preguntar si queremos jugar otra vez.*/
+
+let listaProductos = [
+    {
+      nombreProducto: "Hidratante facial",
+      precio: 3500,
+      categoria: "Hidratante",
+    },
+    { nombreProducto: "Tonico exfoliante", precio: 2350.45, categoria: "Tonico" },
+    {
+      nombreProducto: "Protector solar factor 50",
+      precio: 3300,
+      categoria: "Protector solar",
+    },
+    {
+      nombreProducto: "Protector solar toque seco factor 50",
+      precio: 4100,
+      categoria: "Protector solar",
+    },
+    {
+      nombreProducto: "Protector solar con color factor 50",
+      precio: 3850.5,
+      categoria: "Protector solar",
+    },
+    {
+      nombreProducto: "Gel de limpieza facial",
+      precio: 1740.99,
+      categoria: "Limpieza",
+    },
+    { nombreProducto: "Tonico hidratante", precio: 1250.99, categoria: "Tonico" },
+    { nombreProducto: "Sérum hidratante", precio: 4250.99, categoria: "Sérum" },
+    {
+      nombreProducto: "Exfoliante de azúcar cherry",
+      precio: 1200,
+      categoria: "Labios",
+    },
+    {
+      nombreProducto: "Pads de hidrogel para contorno de ojos",
+      precio: 2800,
+      categoria: "Ojos",
+    },
+    {
+      nombreProducto: "Mascarilla facial",
+      precio: 3250.99,
+      categoria: "Mascaras",
+    },
+    { nombreProducto: "Sérum facial con PHA", precio: 3200, categoria: "Sérum" },
+    { nombreProducto: "Bálsamo labial", precio: 3200, categoria: "Labios" },
+    { nombreProducto: "Crema contorno de ojos", precio: 3200, categoria: "Ojos" },
+    {
+      nombreProducto: "Protector solar en barra",
+      precio: 3800,
+      categoria: "Protector solar",
+    },
+    {
+      nombreProducto: "Mascara de hidratación y reparación",
+      precio: 1200,
+      categoria: "Mascaras",
+    },
+    { nombreProducto: "Agua micellar", precio: 2890, categoria: "Limpieza" },
+  ];
